6a25bc3eea46e38761d80025716515ba4faff907
Garrett Levine <garrlevine@gmail.com>
2017-11-11 19:07:45 -0500

    eslint server

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const path = require('path');
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
-
-const router = require(`router`);
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('router'); // eslint-disable-line
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
-app.use(`/api`, router);
+app.use('/api', router);
 
-app.get(`*`, (req, res, next) => res.sendFile(path.resolve(`./public/index.html`)));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./public/index.html'));
+});
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
-})
+  console.log(`App is running on port ${port}`); // eslint-disable-line
+});
